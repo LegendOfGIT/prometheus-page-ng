b07a355195cf644828c3ba12f80c9ee53eb59fd7
Marschner, Marcel <marcel.marschner@dfb.de>
2023-02-10 16:41:16 +0100

    - search field is now pre-filled, when there is a search pattern in the url

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -33,7 +33,7 @@ export class HeaderComponent {
     this.lastLoggedInFlag = this.userService.isLoggedIn;
 
     const searchPattern = new URL(window.location.href).searchParams.get('search');
-    this.isSearchFieldActive = undefined !== searchPattern && searchPattern !== '';
+    this.isSearchFieldActive = undefined !== searchPattern && searchPattern !== null;
     this.searchPatternControl.setValue(searchPattern);
   }
 
