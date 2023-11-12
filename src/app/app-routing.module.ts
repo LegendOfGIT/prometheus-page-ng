b26e9b6869f178a5a342529acc4591cb7041b9fd
Marcel Marschner <marcel.marschner@dfb.de>
2023-11-12 23:51:11 +0100

    - reverted splitting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ItemsComponent } from './component/overview/items.component';
 import { ProfileComponent } from './component/profile/profile.component';
 import { WishlistItemsComponent } from './component/wishlist/wishlist-items.component';
 import { ImprintComponent } from './component/legal/imprint.component';
+import { StartPageComponent } from './component/landing-pages/start-page.component';
+import { SingleProductViewComponent } from './component/product-view/single-product-view.component';
 import { DataProtectionComponent } from './component/legal/data-protection.component';
 
 const routes: Routes = [
-    { path: 'data-protection', component: DataProtectionComponent },
-    { path: 'imprint', component: ImprintComponent },
-    { path: 'profile', component: ProfileComponent },
-    { path: 'wishlist', component: WishlistItemsComponent },
-    {
-      path: undefined,
-      loadChildren: () => import('./component/product-view/product-view.module').then(m => m.ProductViewModule)
-    },
-    {
-      path: undefined,
-      loadChildren: () => import('./component/overview/product-overview.module').then(m => m.ProductOverviewModule)
-    },
-    {
-      path: undefined,
-      loadChildren: () => import('./component/landing-pages/start-page.module').then(m => m.StartPageModule)
-    }
+  { path: 'data-protection', component: DataProtectionComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'wishlist', component: WishlistItemsComponent },
+  { path: 'p/:itemId/:seoFriendlyProductTitle', component: SingleProductViewComponent },
+  { path: ':navigationIdLevelA', component: ItemsComponent },
+  { path: ':navigationIdLevelA/:navigationIdLevelB', component: ItemsComponent },
+  { path: ':navigationIdLevelA/:navigationIdLevelB/:navigationIdLevelC', component: ItemsComponent },
+  { path: '', component: StartPageComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking'
-})],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,50 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AddToWishlistComponent } from './component/wishlist/add-to-wishlist.component';
+import { ItemComponent } from './component/item/item.component';
+import { ItemsComponent } from './component/overview/items.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { ProfileComponent } from './component/profile/profile.component';
+import { WishlistItemsComponent } from './component/wishlist/wishlist-items.component';
 import { ImprintComponent } from './component/legal/imprint.component';
+import { TranslationPipe } from './pipes/translation.pipe';
+import { StartPageComponent } from './component/landing-pages/start-page.component';
+import { CategoryTeaserComponent } from './component/landing-pages/category-teaser.component';
+import { ModeratedTeaserComponent } from './component/landing-pages/moderated-teaser.component';
+import { SingleProductViewComponent } from './component/product-view/single-product-view.component';
+import { UrlEncodePipe } from './pipes/web.pipe';
 import { GeneralDataProtectionRegulationComponent } from './component/legal/general-data-protection-regulation.component';
 import { DataProtectionComponent } from './component/legal/data-protection.component';
+import { FilterSelectionComponent } from './component/filter/filter-selection.component';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
-import { PipesModule } from './pipes/pipes.module';
-import { SharedComponentsModule } from './component/shared-components.module';
-import { WishlistItemsComponent } from './component/wishlist/wishlist-items.component';
+import { LoadingComponent } from './component/loading/loading.component';
+import { BreadcrumbsComponent } from './component/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
+    AddToWishlistComponent,
+    BreadcrumbsComponent,
+    CategoryTeaserComponent,
+    ModeratedTeaserComponent,
     GeneralDataProtectionRegulationComponent,
     HeaderComponent, FooterComponent,
     DataProtectionComponent,
+    FilterSelectionComponent,
     ImprintComponent,
+    ItemComponent,
+    ItemsComponent,
+    LoadingComponent,
     ProfileComponent,
-    WishlistItemsComponent
+    SingleProductViewComponent,
+    StartPageComponent,
+    WishlistItemsComponent,
+
+    TranslationPipe,
+    UrlEncodePipe
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
@@ -34,9 +57,7 @@ import { WishlistItemsComponent } from './component/wishlist/wishlist-items.comp
     FormsModule,
     HttpClientModule,
     NgxSliderModule,
-    PipesModule,
-    ReactiveFormsModule,
-    SharedComponentsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
