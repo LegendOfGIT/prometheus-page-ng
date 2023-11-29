347d054dfd550986bb5f65ee7d583b47038c8788
Marcel Marschner <marcel.marschner@dfb.de>
2023-11-29 09:40:46 +0100

    - prepared real production release

--- update-and-build-app.sh
+++ update-and-build-app.sh
@@ -1,3 +1,4 @@
 git pull
 docker run -it --rm -w /app -v $(pwd):/app alexsuch/angular-cli:10.2.3 ng build --configuration production
-docker run -it --rm -w /app -v $(pwd):/app alexsuch/angular-cli:10.2.3 ng run prometheus-page:server
+#docker run -it --rm -w /app -v $(pwd):/app alexsuch/angular-cli:10.2.3 ng run prometheus-page:server
+docker run -it --rm -w /app -v $(pwd):/app alexsuch/angular-cli:10.2.3 npm run start
