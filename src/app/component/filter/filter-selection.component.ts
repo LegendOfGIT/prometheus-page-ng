da1c5d1289a78a6ff9013df9cf16204e2807dcf1
Marcel Marschner <marcel.marschner@dfb.de>
2024-03-11 16:56:54 +0100

    - added shop

--- src/app/component/filter/filter-selection.component.ts
+++ src/app/component/filter/filter-selection.component.ts
@@ -261,6 +261,7 @@ export class FilterSelectionComponent implements OnInit, AfterViewChecked {
     new FilterItem('1000043', 'Songmics'),
     new FilterItem('1000259', 'Soundcore'),
     new FilterItem('1000044', 'Studibuch'),
+    new FilterItem('1000260', 'Taipan Schmuck'),
     new FilterItem('1000045', 'Third of Life'),
     new FilterItem('1000046', 'Timber Taste'),
     new FilterItem('1000047', 'Toom'),
