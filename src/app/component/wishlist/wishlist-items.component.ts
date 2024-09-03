eb63f4767917616b2deba6f88fcef8ccce8ac78d
Marcel Marschner <marcel.marschner@dfb.de>
2024-09-03 11:11:35 +0200

    - worked on wishlist items rendering - added delete timers for wishlist and wishlist items

--- src/app/component/wishlist/wishlist-items.component.ts
+++ src/app/component/wishlist/wishlist-items.component.ts
@@ -4,7 +4,6 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
 import {WishlistItemsApiService} from 'src/app/service/wishlist-items-api.service';
 import {Module, NavigationService} from 'src/app/service/navigation.service';
 import {WishlistItem} from '../../model/wishlist-item';
-import {MessagesService} from '../../service/messages.service';
 
 @Component({
   selector: 'app-wishlist-items',
@@ -12,6 +11,9 @@ import {MessagesService} from '../../service/messages.service';
   styleUrls: ['./wishlist-items.component.scss']
 })
 export class WishlistItemsComponent implements OnInit {
+    private deleteTimers: DeleteItemTimer[] = [];
+    private deleteWishlistTimerHandle: any;
+
     constructor(
       private itemsService: WishlistItemsApiService,
       private activatedRoute: ActivatedRoute,
@@ -30,12 +32,46 @@ export class WishlistItemsComponent implements OnInit {
         });
     }
 
-    public removeItem(item: WishlistItem | null): void {
-      this.itemsService.removeItemFromWishlist(item?.id ?? '');
+    public removeItemOrCancelRemove(item: WishlistItem | null): void {
+      const deleteTimer: DeleteItemTimer | undefined = this.deleteTimers
+        .find((deleteTimer: DeleteItemTimer): boolean => deleteTimer.item === item);
+
+      if (deleteTimer) {
+        clearTimeout(deleteTimer.deleteTimerHandle);
+        this.deleteTimers = this.deleteTimers.filter((deleteTimer: DeleteItemTimer): boolean => deleteTimer.item !== item)
+        return;
+      }
+
+      this.deleteTimers.push({
+        deleteTimerHandle: setTimeout(
+          () => this.itemsService.removeItemFromWishlist(item?.id ?? ''),
+          5000),
+        item
+      });
     }
 
-    public deleteWishlist(): void {
-      this.itemsService.deleteWishlist().subscribe(() => this.router.navigate(['/', 'wishlists']));
+    public deleteWishlistOrCancelDelete(): void {
+      if (this.deleteWishlistTimerHandle) {
+        clearTimeout(this.deleteWishlistTimerHandle);
+        this.deleteWishlistTimerHandle = undefined;
+        return;
+      }
+
+      this.deleteWishlistTimerHandle = setTimeout(
+        () => this.itemsService
+          .deleteWishlist()
+          .subscribe(() => this.router.navigate(['/', 'wishlists'])),
+        5000
+      );
+    }
+
+    public isDeletionTimerActive(item: WishlistItem | null): boolean {
+      return this.deleteTimers
+        .find((deleteTimer: DeleteItemTimer): boolean => deleteTimer.item === item) !== undefined;
+    }
+
+    get WishlistDeletionTimerActive(): boolean {
+      return this.deleteWishlistTimerHandle !== undefined;
     }
 
     get items(): Array<WishlistItem | null> {
@@ -49,4 +85,9 @@ export class WishlistItemsComponent implements OnInit {
     get WishlistTitle(): string {
       return this.itemsService.activeWishlist?.title ?? '';
     }
+}
+
+interface DeleteItemTimer {
+  deleteTimerHandle: any;
+  item: WishlistItem | null;
 }
