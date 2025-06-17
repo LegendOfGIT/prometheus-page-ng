78e12f526f4956bacd2f2da1b97034ece87abe4c
Marcel Marschner <marcel.marschner@dfb.de>
2025-06-17 23:17:05 +0200

    - updated startpage

--- src/app/configurations/startpage.ts
+++ src/app/configurations/startpage.ts
@@ -10,7 +10,7 @@ export class Startpage {
       .setSsrRendering(true),
     /*new TeaserItem('/fashion?search=kostüm', 'FASHION', 'kostüm', 'TEASER_HALLOWEEN_COSTUMES')
       .setSsrRendering(true),*/
-    new TeaserItem('/hashtags/Schnäppchen?search=Vater', '', 'Vater', 'TEASER_TITLE_FATHERS_DAY')
+    new TeaserItem('/vehicles/scooters', 'VEHICLES_SCOOTERS', '', 'TEASER_TITLE_SCOOTERS')
       .setHashtags(['Schnäppchen'])
       .setSsrRendering(true),
     /*new TeaserItem('/home/garden', 'HOME_GARDEN', 'grill', 'TEASER_TITLE_INTO_THE_GARDEN'),*/
